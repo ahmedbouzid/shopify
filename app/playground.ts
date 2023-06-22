e526214f3db896ecb73fac39ba5d91be83fa205b
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-22 11:51:09 +0200

    functions

--- app/playground.ts
+++ app/playground.ts
@@ -1,3 +1,11 @@
 export default function play() {
   console.log("Hello World"); 
+    const name = 'Ahmed' ;
+    const age = 27 ;
+ function logPErsonInfo( personName :any , personAge :any) {
+  const info = "name: " + personName + ", age : " + personAge ;
+  console.log(info)
+  return info
+   }
+   logPErsonInfo(name , age)
 }
