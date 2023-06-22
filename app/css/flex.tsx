9d15f9e6cf7fb0d478ee677d918c6ef310490791
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-22 14:57:06 +0200

    some styliing

--- app/css/flex.tsx
+++ app/css/flex.tsx
@@ -2,12 +2,22 @@
 import f from './flex.module.scss'
 const Flex = () => {
     return (
-        <div className={f.parent}>
+<div>
+<div className={f.parent}>
             <div className={f.children}></div>
             <div className={f.children}></div>
             <div className={f.children}></div>
             <div className={f.children}></div>
         </div>
+
+        <div className={f.parent1}>
+            <div className={f.children1}></div>
+            <div className={f.children1}></div>
+            <div className={f.children1}></div>
+            <div className={f.children1}></div>
+        </div>
+</div>
+        
     )
 }
 export default Flex ;
