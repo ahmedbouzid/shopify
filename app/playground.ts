e145b24a194048fd528cb04b12831f4707a9ad79
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-22 14:08:29 +0200

    use function in iinterface

--- app/playground.ts
+++ app/playground.ts
@@ -1,3 +1,5 @@
+import { log } from "console";
+
 /* 
 Ts recommand to use Interfaces over types
 Descripe data structure in more natural way 
@@ -17,9 +19,17 @@ type Person = {
   age:number
 }
 */
+interface personnLogger  {
+  (name : string , age:number): string }
+
 export default function play() {
   console.log("Hello World"); 
-  
+  // methode using erro functon for person logger 
+  const logperson3 :personnLogger = (personName :string , personAge:number) : string => {
+    const info = `Name: ${personName}, age: ${personAge} `
+    console.log('Using errro functions ::::::::::::::::: ',info) ;
+    return info
+  }
     const person : Person = { 
        name : 'Ahmed' ,
        age : 27 ,
@@ -29,6 +39,7 @@ export default function play() {
   console.log(info)
   return info
    }
+   
    function logPerson2(person:Person) { 
     const info = ` Name 1: ${person.name} , age: ${person.age}`
     console.log(info) ;
@@ -36,4 +47,5 @@ export default function play() {
    }
    logPErsonInfo(person.name , person.age)
    logPerson2(person) ;
+   logperson3
 }
