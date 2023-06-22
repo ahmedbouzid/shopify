268d79510ab4c24d42b0413cf6265ae451a9269b
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-22 10:54:29 +0200

    create model for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { log } from "console";
 
 export default function Home() {
   const message : string = "Hello World";
   let age : number = 30 ;
+  let b !: AddEventListenerOptions;
+  let c !: NodeJS.Process ;
+  let person : User ={
+    name :'ahmed',
+  }
   return (
     <div>
       {message}
       <h3>{age}</h3>
+      {person.name}
     </div>
   )
 }
