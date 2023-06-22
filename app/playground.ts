2f2293ad8798b65e2470b2fc0bb82f117838d52a
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-22 11:56:36 +0200

    try with object

--- app/playground.ts
+++ app/playground.ts
@@ -1,11 +1,14 @@
 export default function play() {
   console.log("Hello World"); 
-    const name = 'Ahmed' ;
-    const age = 27 ;
- function logPErsonInfo( personName :any , personAge :any) {
+  
+    const person = { 
+       name : 'Ahmed' ,
+       age : 27 ,
+    }
+ function logPErsonInfo( personName :string , personAge : number) {
   const info = "name: " + personName + ", age : " + personAge ;
   console.log(info)
   return info
    }
-   logPErsonInfo(name , age)
+   logPErsonInfo(person.name , person.age)
 }
